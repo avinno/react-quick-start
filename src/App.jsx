e6cd4ef47f524a5fa39efd83719dc0a355bab6db
Aaron Varga <aaronvarga@onmail.com>
2024-08-08 21:13:54 -0400

    playing with project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import FoodList from './FoodList';
-import Icon from './Icons.jsx';
+// Stylesheets
+import './normalize.css';
+import './index.scss';
 
+// components
+import FoodList from './components/FoodList/FoodList.jsx';
+import Icon from './components/Icons/Icons.jsx';
+
+// App
 function App() {
 
   return (
     <>
         <Icon />
-      <FoodList />
+        <FoodList />
     </>
   )
 }
--- src/components/FoodList/FoodList.jsx
+++ src/components/FoodList/FoodList.jsx
@@ -1,11 +1,14 @@
+// Stylesheet
+import './FoodList.module.scss'
 
-
+// Data
 const products = [
     { title: 'Cabbage', id: 1 },
     { title: 'Garlic', id: 2 },
     { title: 'Apple', id: 3 },
 ];
 
+// Component Function
 export default function FoodList() {
     const listItems = products.map(product => <li key={product.id}>{product.title}</li>);
 
